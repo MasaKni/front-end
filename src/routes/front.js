dd44a2e6d73c0a94fb56c2c48afa92c14c07318e
Masa <masakoni13@gmail.com>
2022-11-01 00:19:54 +0200

    final ip address

--- src/routes/front.js
+++ src/routes/front.js
@@ -4,7 +4,7 @@ const router = express.Router()
 //redirect to catalog server
 router.use('/books/info/:id', async(req, res)=>{
     const id = req.params.id
-    const url = 'http://10.0.2.15:3000/books/info/'+id
+    const url = 'http://192.168.56.101:3000/books/info/'+id
     request({url, json:true}, (error,  {body})=>{
            if(error){
                return res.status(404).send(error)
@@ -15,7 +15,7 @@ router.use('/books/info/:id', async(req, res)=>{
 //redirect to catalog server
 router.use('/books/search/:topic', (req, res)=>{
     const topic = req.params.topic
-    const url = 'http://10.0.2.15:3000/books/search/'+topic
+    const url = 'http://192.168.56.101:3000/books/search/'+topic
     request({url, json:true}, (error,  {body})=>{
            if(error){
                return res.status(404).send(error)
@@ -26,7 +26,7 @@ router.use('/books/search/:topic', (req, res)=>{
 //catalog
 router.patch('/books/:id', (req, res)=>{
     const id = req.params.id
-    const url = 'http://10.0.2.15:3000/books/'+id
+    const url = 'http://192.168.56.101:3000/books/'+id
     request({url, json:true, method: 'PATCH', body: req.body}, (error,  {body})=>{
             if(error){
                return res.status(404).send(error)
@@ -38,7 +38,7 @@ router.patch('/books/:id', (req, res)=>{
 //redirect to order server
 router.use('/books/purchase/:id', (req, res)=>{
     const id = req.params.id
-    const url = 'http://10.0.2.15:3001/books/purchase/'+id
+    const url = 'http://192.168.56.101:3001/books/purchase/'+id
     request({url, json:true}, (error, {body})=>{
         if(error){
             return res.send(error)
