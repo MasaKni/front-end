e164a816bf2473a06ca8ffe1c10601c13e073310
Masa <masakoni13@gmail.com>
2022-10-31 21:24:28 +0200

    fixed ip addresses

--- src/index.js
+++ src/index.js
@@ -2,11 +2,11 @@ const express = require('express')
 const frontRouter = require('./routes/front.js')
 
 const app = express()
-// const PORT = process.env.PORT || 
+const PORT = process.env.PORT || 5000
 
 app.use(express.json())
 app.use(frontRouter)
 
-app.listen(5000, ()=>{
-    console.log('Front server is up and running on port:'+ 5000)
+app.listen(PORT, ()=>{
+    console.log('Front server is up and running on port:'+ PORT)
 })
